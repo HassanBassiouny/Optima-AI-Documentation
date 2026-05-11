8539f9a6f07116b7c6a8d989e01c99534c4b486e
Hassan Kamel Bassiouny <hassan1.bassiony2@gmail.com>
2026-05-11 23:10:40 +0300

    Commit Docs

---
+++ app/docs/layout.jsx
@@ -0,0 +1,16 @@
+import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { source } from '@/lib/source';
+
+export default function Layout({ children }) {
+  return (
+    <DocsLayout
+      tree={source.pageTree}
+      nav={{
+        title: '⚡ OptimaAi Docs',
+        githubUrl: 'https://github.com/your-org/optimaai',
+      }}
+    >
+      {children}
+    </DocsLayout>
+  );
+}
